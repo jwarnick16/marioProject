d9885529172c004178b567fd6c187942c25197ad
J. <138881481+jwarnick16@users.noreply.github.com>
2024-09-13 18:53:10 +0000

    Pending changes exported from your codespace

---
+++ marioProject1/mario.js
@@ -0,0 +1,24 @@
+/* Name: Justine
+Date: 9/13/2024
+Program: Mario Project 1
+Description: Take in user input for how many bricks tall the pyramid should be and then build the pyramid in the console
+*/
+
+userResponse = prompt("How many bricks tall should the pyramid be?");
+printPyramid(userResponse);
+
+
+function printPyramid(height) {
+    let n = userResponse;
+    let string = "";
+    for (let i = 1; i <= n; i++) {
+        for (let j = 0; j < n - i; j++) {
+            string += " ";
+        }
+        for (let k = 0; k < i; k++) {
+            string += "#";
+        }
+        string += "\n";
+    }
+    console.log(string);
+}
