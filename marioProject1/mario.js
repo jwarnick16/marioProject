4ac466de0e50bf1b0cd155bf13b922948a6fdf89
J. <138881481+jwarnick16@users.noreply.github.com>
2024-09-27 20:24:57 -0500

    Update mario.js

--- marioProject1/mario.js
+++ marioProject1/mario.js
@@ -4,21 +4,21 @@ Program: Mario Project 1
 Description: Take in user input for how many bricks tall the pyramid should be and then build the pyramid in the console
 */
 
-userResponse = prompt("How many bricks tall should the pyramid be?");
-printPyramid(userResponse);
+userResponse = prompt("How many bricks tall should the pyramid be?"); // asks user for pyramid height
+printPyramid(userResponse); // inputs response to print pyramid function
 
 
 function printPyramid(height) {
-    let n = userResponse - 1;
-    let string = "";
-    for (let i = 1; i <= n; i++) {
-        for (let j = 0; j < n - i; j++) {
-            string += " ";
+    let n = userResponse; // converts user response into variable n
+    let string = ""; // assigning an empty character value to the variable string
+    for (let i = 1; i <= n - 1; i++) { // starts first loop
+        for (let j = 0; j < n - i; j++) { // starts first inner loop
+            string += " "; 
         }
-        for (let k = 0; k < i; k++) {
+        for (let k = - 1; k < i; k++) { // starts second inner loop
             string += "#";
         }
-        string += "\n";
+        string += "\n"; // puts each string of characters on a new line
     }
-    console.log(string);
+    console.log(string); // prints out pyramid
 }
