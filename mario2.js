513466816523603b5e54ce1f3738e2045535764c
J. <138881481+jwarnick16@users.noreply.github.com>
2024-09-27 22:37:30 -0500

    Update mario2.js

--- mario2.js
+++ mario2.js
@@ -16,7 +16,7 @@ function printPyramid(height) {
         for (let j = 0; j < n - i; j++) { // First inner loop to replace blank spaces with periods so they will not be ignored by HTML
             rowStr += "."; // Adds periods to blank spaces
         }
-        for (let k = 0; k < i; k++) { // Second inner loop, places hashes as bricks based on user input
+        for (let k = - 1; k < i; k++) { // Second inner loop, places hashes as bricks based on user input
             rowStr += "#"; // Assigning hashes as bricks
         }
         container.innerHTML += rowStr + "<br>"; // Creates breaks between each row so pyramid appears stacked
